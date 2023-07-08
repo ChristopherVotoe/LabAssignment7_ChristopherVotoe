b1183e1d003da76e9f17c4dbd50f8b7ccbe867ca
ChristopherV <Chris.Votoe.Official@gmail.com>
2023-07-07 22:57:26 -0400

    idk

--- Bubblesort_Christophervotoe.c
+++ Bubblesort_Christophervotoe.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 
-void bubble(int arr[], int n)
+void bubbleSort(int arr[], int n, int swaps[])
 {
-    int i, j, tmp;
+    int i, j, temp;
+    int totalSwaps = 0;
+
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                tmp = arr[j];
+                temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
+                arr[j + 1] = temp;
+                swaps[j + 1]++;
+                totalSwaps++;
             }
         }
     }
+
+    printf("Swaps needed for each index:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Index %d: %d\n", i, swaps[i]);
+    }
+    printf("Total swaps needed: %d\n", totalSwaps);
 }
 
 int main()
 {
-    int num[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
-    int n = sizeof(num) / sizeof(num[0]);
-    bubble(num, n);
-    printf("Sorted Array\n");
+    int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int swaps[n];
+
+    // Initialize swaps array with 0
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", num[i]);
+        swaps[i] = 0;
     }
+
+    bubbleSort(arr, n, swaps);
+
     return 0;
 }
